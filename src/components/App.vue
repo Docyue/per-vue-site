ee5d031e283054f9bd6decf2d58f264ac9126606
docyue <442497994@qq.com>
2017-05-15 11:13:54 +0800

    新增vue模板

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="main-content">
     <router-link to="/">/首页</router-link>&emsp;
-    <router-link to="/page1">/好文转载</router-link>&emsp;
-    <router-link to="/page2">/风景如画</router-link>&emsp;
-    <!-- <router-link to="/whatever">404</router-link><hr> -->
+    <router-link to="/page1">/列表页1</router-link>&emsp;
+    <router-link to="/page2">/列表页2</router-link>&emsp;
+    <router-link to="/whatever">404</router-link><hr>
     <router-view></router-view>
   </div>
 </template>
