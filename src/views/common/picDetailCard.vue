e2cd4b077b2f7478149d09d60d2fb02d753e27d6
docyue <442497994@qq.com>
2017-05-17 17:06:56 +0800

    更新网站bate版

--- src/views/common/picDetailCard.vue
+++ src/views/common/picDetailCard.vue
@@ -1,8 +1,15 @@
 <template>
   <section class="pic-detail-box">
     <h2 class="head">{{picDetailData.head}}</h2>
-    <p class="title"></p>
-  		hahahhahahahha
+    <p class="title">
+    	<span class="date" :title="picDetailData.date">出游时间:{{picDetailData.date}}</span>
+    </p>
+  	<div class="content">
+  		<div class="card" v-for="item in picDetailData.content">
+  			<img :src="subItem"  v-for="subItem in item.pic" v-if="item.pic.length > 0">
+  			<p class="pic-decription">{{item.description}}</p>
+  		</div>
+  	</div>
   </section>
 </template>
 <script>
@@ -12,4 +19,8 @@ export default {
 </script>
 <style type="text/css">
   .pic-detail-box .head{text-align: center;font-size: 20px;}
+  .pic-detail-box .title .date{display: inline-block;background: rgba(0, 0, 0, .7);color:#fff;font-size: 12px;padding:1px 10px;border-radius: 3px;}
+  .pic-detail-box .content{padding: 20px 0 0 0; text-align: center;}
+  .pic-detail-box .content img {display: block;margin: 0 auto 10px auto;}
+</style>
 </style>
--- src/views/pageFirst/index.vue
+++ src/views/pageFirst/index.vue
@@ -1,10 +1,44 @@
 <template>
-  <div>
-    page1
-  </div>
+  <ul class="art-list">
+  	<li v-for="item in artListdata" class="clearfix" @click="goDetail(item)">
+  		<span class="item-left">{{item.head}}</span>
+  		<span class="item-right">{{item.date}}</span>
+  	</li>
+  </ul>
 </template>
 <script>
+import api from '../../api/index.js'
 export default {
-  name: 'pageFirst'
+  name: 'pageFirst',
+  data () {
+    return {
+      artListdata: []
+    }
+  },
+  mounted () {
+    this.getListData()
+  },
+  methods: {
+    getListData () {
+      let vue = this
+      api.getListData().then(function (data) {
+        vue.artListdata = data.data.pageFirstListData
+      })
+    },
+    goDetail (data) {
+      this.$router.push({
+        path: 'pageDetail',
+        query: {
+          id: data.id,
+          type: data.type
+        }
+      })
+    }
+  }
 }
 </script>
+<style type="text/css">
+	.art-list  li {color:#aaa;}
+	.art-list .item-left{float: left;display: inline-block;width: 80%;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;}
+	.art-list .item-right{}
+</style>
