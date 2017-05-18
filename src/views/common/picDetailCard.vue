fbedca099ffc8727b2fc7130092af97b3df45292
docyue <442497994@qq.com>
2017-05-18 13:52:11 +0800

    更新内容

--- src/views/common/picDetailCard.vue
+++ src/views/common/picDetailCard.vue
@@ -6,8 +6,8 @@
     </p>
   	<div class="content">
   		<div class="card" v-for="item in picDetailData.content">
-  			<img :src="subItem"  v-for="subItem in item.pic" v-if="item.pic.length > 0">
-  			<p class="pic-decription">{{item.description}}</p>
+  			<p class="pic-decription" v-html="item.description"></p>
+        <img :src="subItem"  v-for="subItem in item.pic" v-if="item.pic.length > 0">
   		</div>
   	</div>
   </section>
@@ -20,7 +20,8 @@ export default {
 <style type="text/css">
   .pic-detail-box .head{text-align: center;font-size: 20px;}
   .pic-detail-box .title .date{display: inline-block;background: rgba(0, 0, 0, .7);color:#fff;font-size: 12px;padding:1px 10px;border-radius: 3px;}
-  .pic-detail-box .content{padding: 20px 0 0 0; text-align: center;}
+  .pic-detail-box .content{padding: 20px 0 0 0; width: 50%;margin: 0 auto;}
   .pic-detail-box .content img {display: block;margin: 0 auto 10px auto;}
+  .pic-detail-box .pic-decription {margin: 0 0 15px 0;}
 </style>
 </style>
