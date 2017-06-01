5e1fa8cee52524697483e2276330a6f9b2519728
docyue <442497994@qq.com>
2017-06-01 11:25:20 +0800

    更新压缩

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,10 @@
 <template>
-	<div class="app-content">
-	  <headerCard></headerCard>
-    <p>{{actTitle}}</p>
-	  <section class="content-section">
-	  	 <router-view></router-view>
-	  </section>
-	</div>
+  <div class="app-content">
+    <headerCard></headerCard>
+    <section class="content-section">
+       <router-view></router-view>
+    </section>
+  </div>
 </template>
 <script>
 require('../css/base.css')
@@ -13,10 +12,16 @@ require('../css/main.css')
 import headerCard from './common/headerCard'
 export default {
   name: 'App',
+  created () {
+    let vue = this
+    setTimeout(function () {
+      vue.actTitle()
+    }, 300)
+  },
   components: {
     headerCard
   },
-  computed: {
+  methods: {
     actTitle () {
       let titleData = this.$store.state.titleData
       let len = parseInt(Math.random() * titleData.length) > 0 ? parseInt(Math.random() * titleData.length) : 0
